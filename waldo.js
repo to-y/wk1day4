33afa43bca799cbd9d8f2e78e990d74fbbf46d6c
to-y <yitong922@gmail.com>
2016-09-08 18:06:48 +0000

    forEach change

--- waldo.js
+++ waldo.js
@@ -1,15 +1,22 @@
-// The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] == "Waldo") {
-      found(i);   // execute callback
+
+var list = ["Alice", "Bob", "Waldo", "Winston"]
+
+list.forEach(function findWaldo(arr, i) {
+   if (arr == "Waldo") {
+      actionWhenFound(arr, i);   // execute callback
     }
+
+})
+
+function actionWhenFound(arr, index) {
+  console.log("Found "+ arr + " at index " + index);
+}
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+list.forEach(findBob = (arr,i) => {
+  if (arr == "Bob") {
+    actionWhenFound(arr, i);
   }
 
-}
-
-function actionWhenFound(arr) {
-  console.log("Found him at index " + arr);
-}
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+})
